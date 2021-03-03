72929642ae476ecaef8d36490a1f6c9dad383fbe
rudebway <rudebway@mail.ru>
2021-03-03 13:47:14 +0300

    1.3

--- start.sh
+++ start.sh
@@ -11,29 +11,30 @@ printf 'Выберите действие \n \n'
 printf '1) Установка Виртульных машин \n'
 printf '2) Настройка Виртуальных машин \n'
 printf '\n'
-printf '3) Выход \n'
+printf '3) Выход \n \n'
+
 read KEY
 
 case $KEY in
   1)bash ./script_import
-    sleep 5
+    sleep 2
     clear
     echo "Установка Виртуальных машин"
     printf "\n"
     echo `printf "${GREEN}Done${NC}"`
     printf "\n"
-    sleep 3 ;;
+    sleep 2 ;;
   2)bash ./script_AU_PCI
-    sleep 5
+    sleep 2
     clear
     echo "Настройка Виртуальных машин"
     printf "\n"
     echo `printf "${GREEN}Done${NC}"`
     printf "\n"
-    sleep 3 ;;
+    sleep 2 ;;
   3) clear
     echo "Выход"
-    sleep 3
+    sleep 2
     break;;
 esac
 done
