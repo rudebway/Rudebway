5fc36457f323c955056cc95e4bc678f9b6e5377f
rudebway <rudebway@mail.ru>
2021-03-03 18:27:35 +0300

    ENG ver

--- start.sh
+++ start.sh
@@ -7,11 +7,11 @@ GREEN='\033[1;32m'
 NC='\033[0m'
 chmod +x script_import
 chmod +x script_AU_PCI
-printf 'Выберите действие \n \n'
-printf '1) Установка Виртульных машин \n'
-printf '2) Настройка Виртуальных машин \n'
+printf 'Select an action \n \n'
+printf '1) Import VM \n'
+printf '2) Customization VM \n'
 printf '\n'
-printf '3) Выход \n \n'
+printf '3) Exit \n \n'
 
 read KEY
 
@@ -19,7 +19,7 @@ case $KEY in
   1)bash ./script_import
     sleep 1
     clear
-    echo "Установка Виртуальных машин"
+    echo "Import VM"
     printf "\n"
     echo `printf "${GREEN}Done${NC}"`
     printf "\n"
@@ -27,13 +27,13 @@ case $KEY in
   2)bash ./script_AU_PCI
     sleep 1
     clear
-    echo "Настройка Виртуальных машин"
+    echo "Customization VM"
     printf "\n"
     echo `printf "${GREEN}Done${NC}"`
     printf "\n"
     sleep 1 ;;
   3) clear
-    echo "Выход"
+    echo "Exit"
     sleep 1
     break;;
 esac
