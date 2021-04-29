0f6b264d446776d2cc055d2e5d8377497f93b2ec
rudebway <rudebway@mail.ru>
2021-04-29 18:05:37 +0300

    2904

--- ATOM/update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd ~
-sudo git clone https://github.com/rudebway/Rudebway.git
-cp -r ~/Rudebway/ATOM/* ~
-rm -rf ~/Rudebway/
-chmod +x ATOM.sh
-chmod +x update.sh
-chmod +x ~/NGN/*
--- XSP/start.sh
+++ XSP/start.sh
@@ -1,40 +1,44 @@
 #!/bin/bash
-while true
-do
-clear
-RED='\033[0;31m'
-GREEN='\033[1;32m'
-NC='\033[0m'
-chmod +x script_import
-chmod +x script_AU_PCI
-printf 'Select an action \n \n'
-printf '1) Import VM \n'
-printf '2) Customization VM \n'
-printf '\n'
-printf '3) Exit \n \n'
+while true; do
+  clear
+  GREEN='\033[1;32m'
+  NC='\033[0m'
+  chmod +x script_import
+  chmod +x script_AU_PCI
+  printf 'Select an action \n \n'
+  printf '1) Import VM \n'
+  printf '2) Customization VM \n'
+  printf '\n'
+  printf '3) Exit \n \n'
 
-read KEY
+  read KEY
 
-case $KEY in
-  1)bash ./script_import
+  case $KEY in
+  1)
+    bash ./script_import
     sleep 1
     clear
     echo "Import VM"
     printf "\n"
-    echo `printf "${GREEN}Done${NC}"`
+    echo $(printf "${GREEN}Done${NC}")
     printf "\n"
-    sleep 1 ;;
-  2)bash ./script_AU_PCI
+    sleep 1
+    ;;
+  2)
+    bash ./script_AU_PCI
     sleep 1
     clear
     echo "Customization VM"
     printf "\n"
-    echo `printf "${GREEN}Done${NC}"`
+    echo $(printf "${GREEN}Done${NC}")
     printf "\n"
-    sleep 1 ;;
-  3) clear
+    sleep 1
+    ;;
+  3)
+    clear
     echo "Exit"
     sleep 1
-    break;;
-esac
+    break
+    ;;
+  esac
 done
