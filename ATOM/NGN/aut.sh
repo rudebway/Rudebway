b1da21fdc5daee532ca30c1bdf8a65f0c0c3cffb
rudebway <rudebway@mail.ru>
2021-05-11 10:23:15 +0300

    1105

--- ATOM/NGN/aut.sh
+++ ATOM/NGN/aut.sh
@@ -18,15 +18,19 @@ chk_mtr() {
 chk_gps() {
     IPDFI=$(cat /tmp/ipdfi.tmp)
     local usb="$1"
-    sshpass -p 'Fx566434' ssh admin@$IPDFI 'echo moLD02p | sudo -S rm /dev/ublox'
-    sshpass -p 'Fx566434' ssh admin@$IPDFI 'echo moLD02p | sudo -S rm /dev/leans'
-    sshpass -p 'Fx566434' ssh admin@$IPDFI 'echo moLD02p | sudo -S FtDetect'
     timeout 5s sshpass -p 'Fx566434' ssh admin@$IPDFI "picocom -b 9600 $usb" >/tmp/gps
     GPST=$(wc -c /tmp/gps | awk '{print $1}')
     if [ "$GPST" -gt "590" ]; then
         GPS="GPS работает нормально"
     else
-        GPS="Нет данных с GPS"
+        sshpass -p 'Fx566434' ssh admin@$IPDFI 'echo moLD02p | sudo -S rm /dev/ublox' >/dev/null
+        sshpass -p 'Fx566434' ssh admin@$IPDFI 'echo moLD02p | sudo -S rm /dev/leans' >/dev/null
+        sshpass -p 'Fx566434' ssh admin@$IPDFI 'echo moLD02p | sudo -S FtDetect' >/dev/null
+        if [ "$GPST" -gt "590" ]; then
+            GPS="GPS работает нормально"
+        else
+            GPS="Нет данных с GPS"
+        fi
     fi
     rm -f /tmp/gps
     echo $GPS
