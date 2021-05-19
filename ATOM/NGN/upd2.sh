e22037905f4b2c60368041cfeac72d12c6fe72be
rudebway <rudebway@mail.ru>
2021-05-19 18:10:09 +0300

    1905

--- ATOM/NGN/upd2.sh
+++ ATOM/NGN/upd2.sh
@@ -3,8 +3,8 @@ IPDFI=$(cat /tmp/ipdfi.tmp)
 TMPUPD2="/tmp/cmdupd2.tmp"
 
 ls ~/uuu-korda/old.firmware | grep qspi- | cut -c22-28 >/tmp/table.tmp
-ls ~/uuu-korda/ | grep qspi- | cut -c22-28 >/tmp/table.tmp
-curl -s http://10.78.1.67/Alvarado/ | grep qspi | cut -c31-37 >/tmp/table.tmp
+ls ~/uuu-korda/ | grep qspi- | cut -c22-28 >>/tmp/table.tmp
+curl -s http://10.78.1.67/Alvarado/ | grep qspi | cut -c31-37 >>/tmp/table.tmp
 
 TABLE=$(cat /tmp/table.tmp)
 
@@ -13,7 +13,7 @@ MENU=$(
     x=1
     while [x < "$STRINGS"]
     do
-    STR=$(cat $TABLE | head -n$x | tail -n1)
+    STR=$($TABLE | head -n$x | tail -n1)
     echo "$x $STR "
     x=x+1
     done
