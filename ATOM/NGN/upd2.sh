2c90881b87656e361e660f8e88a05d0751dc258f
rudebway <rudebway@mail.ru>
2021-05-19 18:05:30 +0300

    1905

--- ATOM/NGN/upd2.sh
+++ ATOM/NGN/upd2.sh
@@ -2,7 +2,7 @@
 IPDFI=$(cat /tmp/ipdfi.tmp)
 TMPUPD2="/tmp/cmdupd2.tmp"
 
-ls ~/uuu-korda/old.firmwares | grep qspi- | cut -c22-28 >/tmp/table.tmp
+ls ~/uuu-korda/old.firmware | grep qspi- | cut -c22-28 >/tmp/table.tmp
 ls ~/uuu-korda/ | grep qspi- | cut -c22-28 >/tmp/table.tmp
 curl -s http://10.78.1.67/Alvarado/ | grep qspi | cut -c31-37 >/tmp/table.tmp
 
@@ -11,7 +11,7 @@ TABLE=$(cat /tmp/table.tmp)
 STRINGS=$(wc -l /tmp/table.tmp)
 MENU=$(
     x=1
-    while [x <= $STRINGS]
+    while [x <= "$STRINGS"]
     do
     STR=$(cat $TABLE | head -n$x | tail -n1)
     echo "$x $STR "
