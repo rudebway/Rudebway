7ba1e4ea0ffb63113b071745da996f6c565f992a
rudebway <rudebway@mail.ru>
2021-05-20 10:15:48 +0300

    2005

--- ATOM/NGN/upd2.sh
+++ ATOM/NGN/upd2.sh
@@ -5,7 +5,7 @@ TMPUPD2="/tmp/cmdupd2.tmp"
 ls ~/uuu-korda/old.firmware | grep qspi- | cut -c22-28 >/tmp/table.tmp
 ls ~/uuu-korda/ | grep qspi- | cut -c22-28 >>/tmp/table.tmp
 curl -s http://10.78.1.67/Alvarado/ | grep qspi | cut -c31-37 >>/tmp/table.tmp
-
+sort /tmp/table.tmp | uniq -u > /tmp/table.tmp
 TABLE=$(cat /tmp/table.tmp)
 
 STRINGS=$(wc -l /tmp/table.tmp | awk '{print $1}')
@@ -23,3 +23,6 @@ dialog --title "Обновление" \
 --backtitle "АТОМ $IPDFI" \
 --menu "Выберите тип обновлений" 15 40 9 \
 $MENU 2>$TMPUPD2
+
+
+echo "cat /tmp/table | head -n$(cat $TMPUPD2) | tail -n1 | awk '{print $2}'"
