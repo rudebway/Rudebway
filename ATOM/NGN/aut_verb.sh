75dc1c21971827d7c21b18aa7bd9e28908a782de
rudebway <rudebway@mail.ru>
2021-07-05 17:52:55 +0300

    0507

--- ATOM/NGN/aut_verb.sh
+++ ATOM/NGN/aut_verb.sh
@@ -2,7 +2,7 @@
 chk_mtr() {
     IPDFI=$(cat /tmp/ipdfi.tmp)
     local usb="$1"
-    sshpass -p 'Fx566434' ssh admin@$IPDFI 'echo moLD02p | sudo -S FtDetect' &>/dev/null
+    sshpass -p 'Fx566434' ssh admin@$IPDFI 'echo moLD02p | sudo -S FtDetect' 
     sshpass -p 'Fx566434' ssh admin@$IPDFI 'sudo systemctl stop cortes'
     sshpass -p 'Fx566434' ssh admin@$IPDFI "cortes-lctrl callib Focus 74000 30000 -p $usb" >/tmp/mtrf
     sshpass -p 'Fx566434' ssh admin@$IPDFI "cortes-lctrl callib Diaphragm 2400 5000 -p $usb" >/tmp/mtrd
@@ -24,16 +24,16 @@ chk_gps() {
     if [ "$GPST" -gt "590" ]; then
         GPS="GPS работает нормально"
     else
-        sshpass -p 'Fx566434' ssh admin@$IPDFI 'echo moLD02p | sudo -S rm /dev/ublox' &>/dev/null
-        sshpass -p 'Fx566434' ssh admin@$IPDFI 'echo moLD02p | sudo -S rm /dev/leans' &>/dev/null
-        sshpass -p 'Fx566434' ssh admin@$IPDFI 'echo moLD02p | sudo -S FtDetect' &>/dev/null
+        sshpass -p 'Fx566434' ssh admin@$IPDFI 'echo moLD02p | sudo -S rm /dev/ublox' 
+        sshpass -p 'Fx566434' ssh admin@$IPDFI 'echo moLD02p | sudo -S rm /dev/leans' 
+        sshpass -p 'Fx566434' ssh admin@$IPDFI 'echo moLD02p | sudo -S FtDetect'
         timeout 5s sshpass -p 'Fx566434' ssh admin@$IPDFI "picocom -b 9600 $usb" >/tmp/gps
-        GPST=$(wc -c /tmp/gps | awk '{print $1}') 2>/dev/null
+        GPST=$(wc -c /tmp/gps | awk '{print $1}') 
         if [ "$GPST" -gt "590" ]; then
             GPS="GPS работает нормально"
         else
             timeout 5s sshpass -p 'Fx566434' ssh admin@$IPDFI "picocom -b 9600 /dev/ttyUSB1" >/tmp/gps
-            GPST=$(wc -c /tmp/gps | awk '{print $1}') 2>/dev/null
+            GPST=$(wc -c /tmp/gps | awk '{print $1}') 
             if [ "$GPST" -gt "590" ]; then
                 GPS="GPS работает"
             else
@@ -61,7 +61,7 @@ chk_rdr() {
 
 chk_lic() {
     LICT=$(cat /tmp/atom.tmp)
-    mount -t cifs -o username=root,password=Fx566434 //10.78.9.10/PrOt /serv 2>/dev/null
+    mount -t cifs -o username=root,password=Fx566434 //10.78.9.10/PrOt /serv 
     if [ "$(ls /serv/licenses | grep $LICT)" = "" ]; then
         mkdir /serv/licenses/$LICT
         cd /serv/licenses/$LICT
@@ -89,9 +89,9 @@ dialog --title "Серийный номер АТОМА" --inputbox "Введит
 SDKVER="$(sshpass -p 'Fx566434' ssh admin@$IPDFI "cortes-builder list | grep roadarsdk" | awk '{print $3}' | cut -b 1-5)"
 if [ "$(sshpass -p 'Fx566434' ssh admin@$IPDFI 'ls ~/cortes/cortes')" == "" ]; then
     echo "10" | dialog --title "Автонастройка АТОМа" --gauge "Установка CORTES" 7 70 0
-    sshpass -p 'Fx566434' ssh admin@$IPDFI "bash <(sed 's/sudo/echo moLD02p | sudo -S/g' <(wget -qO- http://10.78.1.67/install_atom.sh))" &>/dev/null
+    sshpass -p 'Fx566434' ssh admin@$IPDFI "bash <(sed 's/sudo/echo moLD02p | sudo -S/g' <(wget -qO- http://10.78.1.67/install_atom.sh))" 
     echo "30" | dialog --title "Автонастройка АТОМа" --gauge "Установка ROADAR SDK" 7 70 0
-    sshpass -p 'Fx566434' ssh admin@$IPDFI "bash <(sed 's/sudo/echo moLD02p | sudo -S/g' <(wget -qO- http://10.78.1.67/update_roadar.sh))" &>/dev/null
+    sshpass -p 'Fx566434' ssh admin@$IPDFI "bash <(sed 's/sudo/echo moLD02p | sudo -S/g' <(wget -qO- http://10.78.1.67/update_roadar.sh))" 
     CRT="CORTES установлен"
     SDKVER="$(sshpass -p 'Fx566434' ssh admin@$IPDFI "cortes-builder list | grep roadarsdk" | awk '{print $3}' | cut -b 1-5)"
     SDK="Версия RoadAR SDK $SDKVER"
@@ -103,7 +103,7 @@ else
         SDK="Версия RoadAR SDK $SDKVER"
     else
         echo "30" | dialog --title "Автонастройка АТОМа" --gauge "Обновление SDK ROADAR" 7 70 0
-        sshpass -p 'Fx566434' ssh admin@$IPDFI "bash <(sed 's/sudo/echo moLD02p | sudo -S/g' <(wget -qO- http://10.78.1.67/update_roadar.sh))" 2>&1 &>/dev/null
+        sshpass -p 'Fx566434' ssh admin@$IPDFI "bash <(sed 's/sudo/echo moLD02p | sudo -S/g' <(wget -qO- http://10.78.1.67/update_roadar.sh))" 
         SDK="Версия RoadAR SDK $SDKVER"
     fi
 fi
@@ -116,7 +116,7 @@ if [ "$TSTUSB" == "01" ]; then
     MTR=$(chk_mtr /dev/leans)
     GPS=$(chk_gps /dev/ublox)
 elif [ "$TSTUSB" == "0" ] || [ "$TSTUSB" == "1" ]; then
-    FTD=$(sshpass -p 'Fx566434' ssh admin@$IPDFI "echo moLD02p | sudo -S rm -f /dev/leans /dev/ublox && sudo FtDetect |grep USB" | awk '{print $3}') &>/dev/null
+    FTD=$(sshpass -p 'Fx566434' ssh admin@$IPDFI "echo moLD02p | sudo -S rm -f /dev/leans /dev/ublox && sudo FtDetect |grep USB" | awk '{print $3}') 
     echo "80" | dialog --title "Автонастройка АТОМа" --gauge "Поиск недостающего USB" 7 70 0
     sleep 1
     if [ "$FTD" == "0" ] || [ "$FTD" == "1" ]; then
