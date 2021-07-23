e5b78e041600ee9a585777cd35a72e2b272d513d
rudebway <rudebway@mail.ru>
2021-07-23 11:12:30 +0300

    2307

--- ATOM/ATOM.sh
+++ ATOM/ATOM.sh
@@ -8,11 +8,12 @@ dialog --title "Работа с АТОМом" \
 --menu "Выберите необходимый тип работ" 15 40 9 \
 1 "Прошивка моторизации" \
 2 "Прошивка БКП" \
-3 "Настройка DFI" \
-4 "Подключение к БКП(БСИ)" \
-5 "Типовые решения" \
-6 "Обновления" \
-7 "Выход" 2>$TMPFCMD
+3 "Прошивка нового iMX" \
+4 "Настройка DFI" \
+5 "Подключение к БКП(БСИ)" \
+6 "Типовые решения" \
+7 "Обновления" \
+8 "Выход" 2>$TMPFCMD
 CMD2RUN=$(cat $TMPFCMD)
 if [ $? -eq "0" ]; then
     case $CMD2RUN in
@@ -23,19 +24,22 @@ if [ $? -eq "0" ]; then
         cd NGN && ./uuu.sh && cd .. && ./ATOM.sh
         ;;
     "3")
-        cd NGN && ./dfi.sh && cd .. && ./ATOM.sh
+        cd NGN && ./uuu2.sh && cd .. && ./ATOM.sh
         ;;
     "4")
-        picocom -b 115200 /dev/ttyUSB0 && ./ATOM.sh
+        cd NGN && ./dfi.sh && cd .. && ./ATOM.sh
         ;;
     "5")
-        cd NGN && ./tbl.sh && cd .. && ./ATOM.sh
+        picocom -b 115200 /dev/ttyUSB0 && ./ATOM.sh
         ;;
     "6")
+        cd NGN && ./tbl.sh && cd .. && ./ATOM.sh
+        ;;
+    "7")
         cd NGN && ./upd.sh && cd .. && ./ATOM.sh
         ;;
 
-    "7")
+    "8")
         exit
         ;;
     esac
